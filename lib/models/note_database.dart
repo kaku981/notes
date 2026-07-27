5506b71f40d212a53c130c32eb5394bea1b09943
kaku981 <153264639+kaku981@users.noreply.github.com>
2026-07-27 14:10:25 +0800

    Start crud firs is the create

--- lib/models/note_database.dart
+++ lib/models/note_database.dart
@@ -5,10 +5,30 @@ import 'package:path_provider/path_provider.dart';
 class NoteDatabase {
   static late Isar isar;
 
+//initialize db
   static Future<void> init() async {
     final dir = await getApplicationDocumentsDirectory();
     isar = await Isar.open(
       [NoteSchema], 
-      directory: dir.path);
+      directory: dir.path,
+      );
   }
+
+//list of notes
+final List<Note> currentNotes = [];
+
+//create - note and save to db
+Future<void> addNote(String textFromUser) async {
+  final newNote = Note()
+    ..text = textFromUser;
+  await isar.writeTxn(() => isar.notes.put(newNote));
+
+  fetchNotes();
+}
+
+//read - notes from db
+
+//update - a note from db
+
+//delete - note from db
 }
