67f2107d2d3c61bfab0096509a34a781586570c2
kaku981 <153264639+kaku981@users.noreply.github.com>
2026-07-28 08:45:05 +0800

    Create a note on the IU

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notes/models/note_database.dart';
+import 'package:provider/provider.dart';
 
 class NotesPage extends StatefulWidget {
   const NotesPage({super.key});
@@ -8,8 +10,45 @@ class NotesPage extends StatefulWidget {
 }
 
 class _NotesPageState extends State<NotesPage> {
+  // access what the user type
+  final textController = TextEditingController();
+
+  // create a note
+  void createNote() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          MaterialButton(
+            onPressed: () {
+              context.read<NoteDatabase>().addNote(textController.text);
+
+              Navigator.pop(context);
+            },
+            child: const Text("Create"),
+          )
+        ],
+      ),
+    );
+  }
+
+  // read notes
+
+  // update a note
+
+  //delete a note
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('Notes')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNote,
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
